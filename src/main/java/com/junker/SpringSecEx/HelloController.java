43f7b3fc60c9bfbc22f2f53e9ce06cc9a6344f73
Joe1Jun <junker91joe@gmail.com>
2024-11-18 16:12:37 +0100

    Added basic getMapping t retriev students. Accessed the csrf token to be used in the header during post requests

---
+++ src/main/java/com/junker/SpringSecEx/HelloController.java
@@ -0,0 +1,21 @@
+package com.junker.SpringSecEx;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@RestController
+
+public class HelloController {
+	
+	@GetMapping("/")
+	public String greeting (HttpServletRequest request) {
+		
+		return "Welcome to here" + " " + request.getSession().getId();
+	}
+	
+	
+
+}
---
+++ src/main/java/com/junker/SpringSecEx/StudentController.java
@@ -0,0 +1,44 @@
+package com.junker.SpringSecEx;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class StudentController {
+
+	// A list of students that acts as an in-memory database for this example.
+    private List<Student> students = new ArrayList<>(List.of(
+        new Student(1, "Joe", 60),
+        new Student(2, "Kevin", 65)
+    ));
+
+    // Endpoint to retrieve the list of students.
+    @GetMapping("/students") // Maps GET requests to "/students".
+    public List<Student> getStudents() {
+        return students; // Returns the current list of students as a JSON response.
+    }
+
+    // Endpoint to retrieve the CSRF token.
+    @GetMapping("/csrf-token") // Maps GET requests to "/csrf-token".
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        // Accesses the CSRF token from the HttpServletRequest attributes.
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    // Endpoint to add a student to the list.
+    @PostMapping("/students") // Maps POST requests to "/students".
+    public Student addStudent(@RequestBody Student student) {
+        // Deserializes the JSON request body into a Student object and adds it to the list.
+        students.add(student);
+        return student;
+    }
+	
+}
